894bcb32e730b66994a72bd285a7b5226dacbf4d
GrimmXoXo <dragoonking2001@gmail.com>
2024-07-10 11:49:39 +0530

    Final Update to publish.yaml(hopefully)

--- Project/setup.py
+++ Project/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='financial_model_taiwan',
-    version='1.0.2',
+    version='1.0.3',
     description='A financial model package trained on data from Taiwan fiscal year 1999-2009',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
